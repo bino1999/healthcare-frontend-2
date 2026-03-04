ca7ff73f370ecf8ad22eefe4f74d158d241b6e27
binodbandara <binod.bandara@ivedha.com>
2026-03-04 07:25:12 +0530

    paf changes

--- src/pages/GeneratePAF/pafApi.js
+++ src/pages/GeneratePAF/pafApi.js
@@ -1,7 +1,7 @@
 // src/pages/GeneratePAF/pafApi.js
 
-const PAF_API_URL = 'http://192.168.8.104:8000/api/generate-pdf';
-// const PAF_API_URL = 'http://100.64.177.106:5678/webhook/directus-data';
+// const PAF_API_URL = 'http://192.168.8.104:8000/api/generate-pdf';
+const PAF_API_URL = 'http://100.64.177.106:5678/webhook/directus-data';
 const DIRECTUS_URL = import.meta.env.VITE_DIRECTUS_URL || 'http://100.64.177.106:8055';
 
 /**
